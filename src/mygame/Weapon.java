e08a7f9c37ab1650d148b7aca728885a41c57571
Vahid Ra <vahid18u@gmail.com>
2014-02-17 16:38:20 +0330

    test

--- src/mygame/Weapon.java
+++ src/mygame/Weapon.java
@@ -1,14 +1,26 @@
 package mygame;
-
+///////v
+import com.jme3.app.Application;
 import com.jme3.asset.AssetManager;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 
-public abstract class Weapon {
+public class Weapon {
+    Application app;
+    Node rootNode;
+    private FireBehaviour fireBehaviour;
+    private Bullet bullet;
+    private int bulletCount;
 
-    FireBehaviour fireBehaviour;
-    int bulletCount;
+    public void setFireBehaviour(FireBehaviour fireBehaviour) {
+        this.fireBehaviour = fireBehaviour;
+    }
+
+    public void setBullet(Bullet bullet) {
+        this.bullet = bullet;
+    }
+
     
     //AudioNode sound;
     //   private Node weaponNode;
@@ -20,13 +32,15 @@ public abstract class Weapon {
 //        this.bulletCount = bulletCount;
 //    }
 
-    public Weapon(FireBehaviour fireBehaviour, int bulletCount) {
+    public Weapon(Application app,Node rootNode,int bulletCount) {
         //       sound = new AudioNode(assetManager,"Sounds/weapon1.wav",false);
-        
-        this.fireBehaviour = fireBehaviour;
-        this.bulletCount = bulletCount;
-    }
+      this.app = app;
+      this.rootNode = rootNode;
+      this.bulletCount = bulletCount;
+      this.fireBehaviour = new FireBehaviourSimple(app,rootNode, bullet);// default
+     // this.bullet = new BulletSimple();
 
+    }
     public void fire() {
         if (bulletCount != 0) {
             fireBehaviour.fire(new Vector3f(1, -1, 1), new Vector3f(1, -1, 1));
@@ -35,7 +49,6 @@ public abstract class Weapon {
             noBullet();
         }
     }
-
     private void noBullet() {
         // play sound of no bullet for example or something else
     }
@@ -47,4 +60,5 @@ public abstract class Weapon {
     public void setBulletCount(int bulletCount) {
         this.bulletCount = bulletCount;
     }
+    
 }
